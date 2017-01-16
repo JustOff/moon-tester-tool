28b03675470f181dddd21a6a60b199e08ef2ce49
JustOff <Off.Just.Off@gmail.com>
2017-01-16 12:13:04 +0200

    simple install

--- bootstrap.js
+++ bootstrap.js
@@ -2,9 +2,39 @@
 
 var {classes: Cc, interfaces: Ci, utils: Cu} = Components;
 Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/AddonManager.jsm");
 
-function main(aSubject) {
-	Cu.reportError("Run!");
+function installFromFile(aWindow, aFile) {
+	function doInstall(aInstall) {
+		var installs = [ aInstall ];
+		var webInstaller = Cc["@mozilla.org/addons/web-install-listener;1"]
+							.getService(Ci.amIWebInstallListener);
+		var browserElement = aWindow.QueryInterface(Ci.nsIInterfaceRequestor)
+							.getInterface(Ci.nsIDocShell).chromeEventHandler;
+		webInstaller.onWebInstallRequested(browserElement, aWindow.document.documentURIObject,
+											installs, installs.length);
+	}
+
+	AddonManager.getInstallForFile(aFile, function(aInstall) {
+		doInstall(aInstall);
+	});
+}
+
+function main(aWindow) {
+	const nsIFilePicker = Ci.nsIFilePicker;
+	var fp = Cc["@mozilla.org/filepicker;1"].createInstance(nsIFilePicker);
+	fp.init(aWindow, "Select add-on to install", nsIFilePicker.modeOpen); 
+	try {
+		fp.appendFilter("Add-ons", "*.xpi");
+		fp.appendFilters(nsIFilePicker.filterAll);
+	} catch (e) {}
+
+	if (fp.show() != nsIFilePicker.returnOK) {
+		return;
+	}
+
+	var file = fp.file;
+	installFromFile(aWindow, file);
 }
 
 var moonttoolObserver = {

e9a2d35db45793ea502238607232a3a66cc96017
JustOff <Off.Just.Off@gmail.com>
2021-02-22 17:47:03 +0200

    Set up release automation

---
+++ release.cmd
@@ -0,0 +1,9 @@
+@echo off
+set VER=2.1.1
+
+sed -i -E "s/version>.+?</version>%VER%</" install.rdf
+sed -i -E "s/version>.+?</version>%VER%</; s/download\/.+?\/moon-tester-tool-.+?\.xpi/download\/%VER%\/moon-tester-tool-%VER%\.xpi/" update.xml
+
+set XPI=moon-tester-tool-%VER%.xpi
+if exist %XPI% del %XPI%
+zip -r9q %XPI% * -x .git/* .gitignore update.xml LICENSE README.md *.cmd *.xpi *.exe

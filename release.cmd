ecbbcd252056ef68b406ab52ffea27eebbf9a386
JustOff <Off.Just.Off@gmail.com>
2021-07-24 20:00:06 +0300

    Bump version

--- release.cmd
+++ release.cmd
@@ -1,5 +1,5 @@
 @echo off
-set VER=2.1.3
+set VER=2.1.4
 
 sed -i -E "s/version>.+?</version>%VER%</" install.rdf
 sed -i -E "s/version>.+?</version>%VER%</; s/download\/.+?\/moon-tester-tool-.+?\.xpi/download\/%VER%\/moon-tester-tool-%VER%\.xpi/" update.xml
